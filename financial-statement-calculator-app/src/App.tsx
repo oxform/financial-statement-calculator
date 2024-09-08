077477bb487734e67a4d8e4fc4d690b9a40ff56e
Blake Schlage <blake.schlage@auxilis.com.au>
2024-09-09 00:18:37 +1000

    Add github icon in the top right

--- financial-statement-calculator-app/src/App.tsx
+++ financial-statement-calculator-app/src/App.tsx
@@ -1,10 +1,21 @@
 import "./App.css";
 import "./globalStyles.scss";
 import SampleList from "./components/SampleList";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
     <div className="App">
+      <a
+        href="https://github.com/oxform/financial-statement-calculator"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="absolute top-4 right-4 text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+        aria-label="View source on GitHub"
+      >
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+      </a>
       <SampleList />
     </div>
   );
